65755705a38a9d1aa2655ff8bcb249b4bf70be7a
MohamadHelli <mou.hamad123@hotmail.com>
2022-05-25 10:42:58 +0200

    Basic

---
+++ Mohamad/Basic/script.js
@@ -0,0 +1,9 @@
+let a = "hello from the consol";
+console.log(a);
+
+var name = 'Martin';
+var age = 25;
+console.log('Hey my ' + name + ' an my ' + age + ' is ');
+
+let player = ["Martin", "Thomas", "Peter2", "Mathias", "Niki"]
+console.log("The most valuable player of the match is " + player[3]);
