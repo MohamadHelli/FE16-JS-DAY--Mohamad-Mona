a7d03d4bbddb019af361227e05b9de7771da811b
MonaFahham <monafahham85@gmail.com>
2022-05-25 15:53:00 +0200

    Final project

--- Mona/Basic/script.js
+++ Mona/Basic/script.js
@@ -1,8 +1,7 @@
 let name = "Martin";
 let age = 25;
+console.log("Hi my name is " + name + " and I am " + age + " years old");
 console.log(`Hi my name is ${name} and I am ${age} years old`);
-// console.log("Hi my name is " + name + " and I am " + age + " years old");
 
-let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
-console.log("The most valuable player of the match is " + players[3]);
-// document.write("The most valuable player of the match is " + players[3]);
+// let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+// console.log("The most valuable player of the match is " + players[3]);
