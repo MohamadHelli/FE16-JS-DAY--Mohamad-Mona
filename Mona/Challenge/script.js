5cb6abf81ed8734ac09ebde14b1d3e11b3ef6526
MonaFahham <monafahham85@gmail.com>
2022-05-25 13:51:33 +0200

    Challenge

---
+++ Mona/Challenge/script.js
@@ -0,0 +1,8 @@
+// let sentence =
+//     "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+
+// console.log(sentence.split("$").join(" "));
+// console.log(sentence.split("$").join(", "));
+
+let nameMiddle = ["Mona", "Mohammad", "Sara", "john", "Gabriel"];
+console.log(nameMiddle[Math.floor((nameMiddle.length - 1) / 2)]);
