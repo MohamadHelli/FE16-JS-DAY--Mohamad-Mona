cd924dbe198953f4571d545452f0c54cfc749728
MohamadHelli <mou.hamad123@hotmail.com>
2022-05-25 11:18:15 +0200

    cool

---
+++ Mohamad/Intermediate/script.js
@@ -0,0 +1,16 @@
+let cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+console.log(cars);
+console.log(cars.sort());
+
+let f = ["apple", "banana", "kiwi"];
+console.log(f);
+f.push("orange")
+console.log(f)
+
+let a = ["monkey", "horse", "dog"]
+console.log(a.reverse())
+a.unshift("cat")
+console.log(a)
+
+let list = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+console.log(list.split("/").join("\n"));
